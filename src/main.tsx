632e1cb308df62fbf15240db66cb0a642c51f0de
abderrahman zerouali <lopikola11@gmail.com>
2026-04-04 11:49:16 +0200

    Fix rendering method for createRoot in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 
 import Lola from './App.js'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Lola/>
   </StrictMode>,
