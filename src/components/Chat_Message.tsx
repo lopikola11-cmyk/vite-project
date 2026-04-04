0a1d1d6c79d09056b05f26b344baf40b6f41ac32
abderrahman zerouali <lopikola11@gmail.com>
2026-04-04 11:30:19 +0200

    Add Chat_Message component for displaying chat messages

---
+++ src/components/Chat_Message.tsx
@@ -0,0 +1,23 @@
+   
+ import RobotPicture from'../assets/robot.png'
+      import UserPicture from'../assets/user.png'
+   import './Chat_Message.css';
+    export type chatMessages1={ 
+id:string,
+sender:string,
+message:string
+   }
+
+   
+   export const Chat_Message = ({ chatMessages}:{ chatMessages:chatMessages1[] }) => (
+  
+        <div className='div-chatMessages'>
+          {chatMessages.map(chat => (
+            <div key={chat.id} className={chat.sender==='robot'?'robot':'user'}>
+              {chat.sender==='robot' && <img src={RobotPicture} width="30"/>}
+              {chat.message}
+              {chat.sender==='user' && <img src={UserPicture} width="30"/>}
+            </div>
+          ))}
+        </div>
+      );
